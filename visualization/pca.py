6154e3d0f85952d32477a839c10aa5e81826bd01
achigbrow <achigbrow@unm.edu>
2022-05-06 14:34:36 -0600

    refactor: fine tuning visualizations

--- visualization/pca.py
+++ visualization/pca.py
@@ -4,8 +4,8 @@ from utils import get_classes
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from visualization.mfccs import get_single_mfcc_features
-from visualization.mfccs import get_processed_mfccs
+import librosa
+import os
 
 
 def get_pca(features, n):
@@ -16,8 +16,49 @@ def get_pca(features, n):
     scaler.fit(transformed)
     return scaler.transform(transformed)
 
+def plot_pca(file_list, directory):
+    genres = {
+        '0': "Rock",
+        '1': "Pop",
+        '2': "Folk",
+        '3': "Instrumental",
+        '4': "Electronic",
+        '5': "Hip-Hop"
+    }
 
-def plot_pca(classification, features):
+    columns = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+
+    for i in range(len(file_list)):
+        g = file_list[i, 1]
+        title = "{0:s} PCAs".format(genres[g])
+
+        filepath = os.path.join(directory, file_list[i, 0])
+        pcm_data, sr = librosa.load(filepath, offset=15.0, duration=60.0)
+
+        pcm_data = pcm_data.reshape(1, -1)
+        pca_data = get_pca(pcm_data, 1)
+
+        df = pd.DataFrame(pca_data, columns=columns)
+
+        plt.plot(df['0'], label='PCA 0', color='crimson')
+        plt.plot(df['1'], label='PCA 1', color='c')
+        plt.plot(df['2'], label='PCA 2', color='skyblue')
+        plt.plot(df['3'], label='PCA 3', color='dodgerblue')
+        plt.plot(df['4'], label='PCA 4', color='slategrey')
+        plt.plot(df['5'], label='PCA 5', color='darkblue')
+        plt.plot(df['6'], label='PCA 6', color='slateblue')
+        plt.plot(df['7'], label='PCA 7', color='rebeccapurple')
+        plt.plot(df['8'], label='PCA 8', color='darkviolet')
+        plt.plot(df['9'], label='PCA 9', color='violet')
+        plt.plot(df['10'], label='PCA 10', color='fuchsia')
+        plt.plot(df['11'], label='PCA 11', color='deeppink')
+        plt.plot(df['12'], label='PCA 12', color='cadetblue')
+
+        plt.title(title)
+        plt.show()
+
+
+def scatter_plot_pca(classification, features):
     pca = get_pca(features, 2)
     df = pd.DataFrame(pca, columns=["Comp1", "Comp2"])
     df["Classification"] = classification
@@ -43,11 +84,17 @@ def plot_pca(classification, features):
 
 
 if __name__ == "__main__":
-    classifications = np.array(get_classes(r"D:\proj3_data\project3\train.csv"))
-    # classifications = np.array(get_classes(r"D:\repos\CS529_Project3\train1.csv"))
-    classification = classifications[:, 1]
-    directory = r"D:\proj3_data\project3\trainwav"
-    features1 = get_single_mfcc_features(classifications, directory)
-    features2 = get_processed_mfccs(directory, classifications)
-    plot_pca(classification, features1)
-    plot_pca(classification, features2)
+    # classifications = np.array(get_classes(r"D:\proj3_data\project3\train.csv"))
+    # # classifications = np.array(get_classes(r"D:\repos\CS529_Project3\train1.csv"))
+    # classification = classifications[:, 1]
+    # directory = r"D:\proj3_data\project3\trainwav"
+    # features1 = get_single_mfcc_features(classifications, directory)
+    # features2 = get_processed_mfccs(directory, classifications)
+    # plot_pca(classification, features1)
+    # plot_pca(classification, features2)
+
+    file_list = get_classes(r"D:\repos\CS529_Project3\examples.csv")
+    file_list = np.array(file_list)
+    print(file_list.shape)
+    train_dir = r"D:\proj3_data\project3\trainwav"
+    plot_pca(file_list, train_dir)
